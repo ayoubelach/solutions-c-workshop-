0cf11b2a131e56f2dde3f6836f6c53902231211d
ayoub <elachkaouiayyoub@gmail.com>
2024-09-28 17:25:37 +0200

    added function challenges folder

---
+++ Day02/fonctions/challenge1.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+int somme(int a , int b){
+
+return a+b;
+
+}
+int main()
+{
+    int n,i;
+
+printf("entrez deux nombre pour calculer la somme : ",somme(n,i));
+
+ scanf(" %d %d",&n,&i);
+printf("la somme est : %d",somme(n,i));
+}
---
+++ Day02/fonctions/challenge3.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+int produit(int a , int b){
+
+return a*b;
+
+}
+int main()
+{
+    int n,i;
+
+printf("entrez deux nombre pour calculer multiplication de pnombre1 fois nombre2 : ");
+
+ scanf(" %d %d",&n,&i);
+printf("multiplication est egal a : %d",produit(n,i));
+}
+
